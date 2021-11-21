48ccaaa5c63525f03be18e7f4f15ba972e2d2fff
Angie <amaticorena103@hotmail.com>
2021-11-21 13:10:57 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function createToDo(todo) {
+  let li = document.createElement('li')
+  li.textContent = `${todo} `
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.appendChild(btn)
+  btn.addEventListener('click', handleDelete)
+  document.querySelector('#tasks').appendChild(li)
+  
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
